937644ee73352bb08e6efb220712a5af3fe149a5
E.S <esimon.1d@gmail.com>
2025-10-16 22:43:59 +0200

    layout modifications

--- src/app/sections/s2.jsx
+++ src/app/sections/s2.jsx
@@ -3,15 +3,21 @@ import React from "react";
 export default function S2() {
   return (
     <section id="s2">
-      <div className="container">
-      <h2>Section 2</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
-        provident aliquam repellendus totam, nesciunt facilis numquam magnam
-        iusto, officia odio laudantium tempora iste ipsa neque doloremque,
-        aspernatur consectetur dignissimos repudiandae.
-      </p>
-      </div>
+        <div className="content bg-amber-700">
+          <h2>Section 2</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptatibus provident aliquam repellendus totam, nesciunt facilis
+            numquam magnam iusto, officia odio laudantium tempora iste ipsa
+            neque doloremque, aspernatur consectetur dignissimos repudiandae.
+          </p>
+        </div>
+        <div className="content bg-amber-800">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            Voluptatibus provident aliquam repellendus totam, nesciunt facilis
+            numquam magnam iusto, officia odio laudantium tempora iste ipsa
+            neque doloremque, aspernatur consectetur dignissimos repudiandae.</p>
+        </div>
     </section>
   );
 }
