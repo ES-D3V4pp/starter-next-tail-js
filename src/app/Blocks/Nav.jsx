0bf0d6ba1d075f60faf267a1a9635c778ae54a76
E.S <esimon.1d@gmail.com>
2025-10-23 23:32:52 +0200

    add styles and logic

--- src/app/Blocks/Nav.jsx
+++ src/app/Blocks/Nav.jsx
@@ -1,10 +1,16 @@
-import { useSpaNavScroll } from '../hooks/useSpaNavScroll';
+import {useState} from 'react';
+import { useSpaNavScroll} from '../hooks/useSpaNavScroll';
 import { routes } from '../config/routes';
 import Logo from '../components/Logo';
 import { Menu } from 'lucide-react';
 
 export default function Nav() {
   const { activePath, scrollTo } = useSpaNavScroll(routes);
+  const [ menuDisplay, setMenuDisplay] = useState('hidden');
+
+  const displayMobileNav = () => {
+    setMenuDisplay(prev => (prev === 'hidden' ? '' : 'hidden'));
+  }
 
   const isActive = (pathname) => activePath === pathname ? 'active' : '';
   const goTo = (target, pathname) => (e) => {
@@ -18,22 +24,24 @@ export default function Nav() {
         <a
           href="/"
           onClick={goTo('hero', '/')}>
-            <Logo style={{ color: 'var(--primary)'}} height="20px"/>
+            <Logo color="white" height="20px"/>
         </a>
-        <ul>
-        {routes.map((link) => (
-          <li key={link.pathname}>
-          <a
-            href={link.pathname}
-            className={isActive(link.pathname)}
-            onClick={goTo(link.target, link.pathname)}
-          >
-            {link.label}
-          </a>
-          </li>
-        ))}
-        </ul>
+        <ul className={menuDisplay}>
+  {routes.map((link) => (
+    <li key={link.pathname}>
+      <a
+        href={link.pathname}
+        className={isActive(link.pathname)}
+        onClick={goTo(link.target, link.pathname)}
+      >
+        {link.label}
+      </a>
+    </li>
+  ))}
+</ul>
+        <button className='block md:hidden' onClick={displayMobileNav}>
          <Menu style={{ color: 'var(--secondary)' }} className='w-6 h-6'/>
+         </button>
       </nav>
     </header>
   );
