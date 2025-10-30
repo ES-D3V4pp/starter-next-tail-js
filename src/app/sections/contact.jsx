3e719be7e41631e558ba3e4dddd742ff47a6a2fb
E.S <esimon.1d@gmail.com>
2025-10-30 14:47:07 +0100

    form.css

--- src/app/sections/contact.jsx
+++ src/app/sections/contact.jsx
@@ -6,19 +6,12 @@ export default function Contact() {
       <div className="container">
         <div className="content">
       <h2>Section Contact</h2>
-      <img src="https://img.freepik.com/vecteurs-libre/formes-conception-degradee-fond-sombre_23-2148426669.jpg?t=st=1761344324~exp=1761347924~hmac=562a282f0a28f495f351f088098cbb1f8b7a2491f27677617a6fcb53a938a2ae&w=2000" alt="image description" />
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
-        provident aliquam repellendus totam, nesciunt facilis numquam magnam
-        iusto, officia odio laudantium tempora iste ipsa neque doloremque,
-        aspernatur consectetur dignissimos repudiandae.
-      </p>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
-        provident aliquam repellendus totam, nesciunt facilis numquam magnam
-        iusto, officia odio laudantium tempora iste ipsa neque doloremque,
-        aspernatur consectetur dignissimos repudiandae.
-      </p>
+    
+    
+      <form>
+          <label htmlFor="fname">First name:</label>
+          <input type="text" id="fname" name="fname" />
+      </form>
       </div>
       </div>
     </section>
--- src/app/sections/s3.jsx
+++ src/app/sections/s3.jsx
@@ -10,6 +10,7 @@ export default function S3() {
             Officia odio laudantium tempora iste ipsa neque doloremque,
         aspernatur consectetur dignissimos repudiandae
           </p>
+          <img src="https://img.freepik.com/vecteurs-libre/formes-conception-degradee-fond-sombre_23-2148426669.jpg?t=st=1761344324~exp=1761347924~hmac=562a282f0a28f495f351f088098cbb1f8b7a2491f27677617a6fcb53a938a2ae&w=2000" alt="image description" />
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
         provident aliquam repellendus totam, nesciunt facilis numquam magnam
